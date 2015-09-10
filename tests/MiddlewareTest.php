9b5eb20f2a45bbe40782289bf181635d9c8e852e
Maksim Naumov <maskim.naumov@hitmeister.de>
2015-09-10 17:29:14 +0200

    Middleware sign request test

---
+++ tests/MiddlewareTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Hitmeister\Component\Api\Tests;
+
+use Hitmeister\Component\Api\Middleware;
+
+class MiddlewareTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * This method is called after a test is executed.
+	 */
+	public function tearDown()
+	{
+		\Mockery::close();
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 */
+	public function testSignRequest()
+	{
+		/** @var \Mockery\Mock $securityHelper */
+		$securityHelper = \Mockery::mock('alias:\Hitmeister\Component\Api\Helper\Security');
+		$securityHelper->shouldReceive('signRequest')->withArgs([
+			'client_secret',
+			'POST',
+			\Mockery::any(),
+			'the_body',
+			\Mockery::any(),
+		])->andReturn('the_signature');
+
+		/** @var \Mockery\Mock $coreHelper */
+		$coreHelper = \Mockery::mock('alias:\GuzzleHttp\Ring\Core');
+		$coreHelper->shouldReceive('url')->withArgs([
+			\Mockery::type('array')
+		]);
+
+		// Fake handler
+		$handler = function(array $request){
+			return $request;
+		};
+
+		$middleware = Middleware::signRequest($handler, 'client_key', 'client_secret');
+		$result = $middleware([
+			'http_method' => 'POST',
+			'body' => 'the_body',
+			'headers' => [],
+		]);
+
+		$this->assertTrue(is_array($result));
+		$this->assertArrayHasKey('headers', $result);
+		$this->assertTrue(is_array($result['headers']));
+		$this->assertArrayHasKey('HM-Client', $result['headers']);
+		$this->assertArrayHasKey('HM-Timestamp', $result['headers']);
+		$this->assertArrayHasKey('HM-Signature', $result['headers']);
+		$this->assertEquals('the_signature', $result['headers']['HM-Signature']);
+		$this->assertEquals('client_key', $result['headers']['HM-Client']);
+	}
+}
