62bf3b735f452c61b6c0b6c6dd4182ef5cac323e
Maksim Naumov <fromyukki@gmail.com>
2015-09-21 12:31:48 +0200

    Categories endpoint: find

--- src/Namespaces/CategoriesNamespace.php
+++ src/Namespaces/CategoriesNamespace.php
@@ -2,7 +2,9 @@
 
 namespace Hitmeister\Component\Api\Namespaces;
 
+use Hitmeister\Component\Api\Cursor;
 use Hitmeister\Component\Api\Endpoints\Categories\Decide;
+use Hitmeister\Component\Api\Endpoints\Categories\Find;
 use Hitmeister\Component\Api\Endpoints\Categories\Get;
 use Hitmeister\Component\Api\Exceptions\InvalidArgumentException;
 use Hitmeister\Component\Api\Exceptions\ResourceNotFoundException;
@@ -23,6 +25,36 @@ use Hitmeister\Component\Api\Transfers\CategoryWithEmbeddedTransfer;
 class CategoriesNamespace extends AbstractNamespace
 {
 	/**
+	 * @param string $q
+	 * @param int    $idParent
+	 * @param int    $limit
+	 * @param int    $offset
+	 * @return Cursor|CategoryTransfer[]
+	 */
+	public function find($q = null, $idParent = null, $limit = null, $offset = 0)
+	{
+		$params = [];
+
+		if (null !== $q) {
+			$params['q'] = $q;
+		}
+		if (null !== $idParent) {
+			$params['id_parent'] = $idParent;
+		}
+		if (null !== $limit) {
+			$params['limit'] = $limit;
+		}
+		if (null !== $offset) {
+			$params['offset'] = $offset;
+		}
+
+		$endpoint = new Find($this->getTransport());
+		$endpoint->setParams($params);
+
+		return new Cursor($endpoint, '\Hitmeister\Component\Api\Transfers\CategoryTransfer');
+	}
+
+	/**
 	 * @param array|CategoryDecideTransfer $data
 	 * @return array|CategoryTransfer[]
 	 */
@@ -42,7 +74,7 @@ class CategoriesNamespace extends AbstractNamespace
 		Response::checkBody($resultRequest);
 
 		$result = [];
-		foreach($resultRequest['json'] as $item) {
+		foreach ($resultRequest['json'] as $item) {
 			$result[] = CategoryTransfer::make($item);
 		}
 
@@ -68,7 +100,7 @@ class CategoriesNamespace extends AbstractNamespace
 
 		try {
 			$result = $endpoint->performRequest();
-		} catch(ResourceNotFoundException $e) {
+		} catch (ResourceNotFoundException $e) {
 			return null;
 		}
 
--- tests/Namespaces/CategoriesNamespaceTest.php
+++ tests/Namespaces/CategoriesNamespaceTest.php
@@ -7,8 +7,56 @@ use Hitmeister\Component\Api\Namespaces\CategoriesNamespace;
 use Hitmeister\Component\Api\Tests\TransportAwareTestCase;
 use Hitmeister\Component\Api\Transfers\CategoryDecideTransfer;
 
+/**
+ * Class CategoriesNamespaceTest
+ *
+ * @category PHP-SDK
+ * @package  Hitmeister\Component\Api\Tests\Namespaces
+ * @author   Maksim Naumov <maksim.naumov@hitmeister.de>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://www.hitmeister.de/api/v1/
+ */
 class CategoriesNamespaceTest extends TransportAwareTestCase
 {
+	public function testFind()
+	{
+		$this->transport
+			->shouldReceive('performRequest')
+			->once()
+			->withArgs([
+				'GET',
+				'categories/',
+				['q' => 'handy', 'id_parent' => 38441, 'limit' => 1, 'offset' => 2],
+				\Mockery::any(),
+				\Mockery::any(),
+			])
+			->andReturn([
+				'headers' => [
+					'Hm-Collection-Range' => ['3-3/10'],
+				],
+				'json' => [
+					[
+						'id_category' => 30511,
+						'id_parent_category' => 38441,
+						'name' => 'schutzfolien-handys'
+					]
+				]
+			]);
+
+		$namespace = new CategoriesNamespace($this->transport);
+		$result = $namespace->find('handy', 38441, 1, 2);
+
+		$this->assertInstanceOf('\Iterator', $result);
+		$result = iterator_to_array($result);
+
+		/** @var \Hitmeister\Component\Api\Transfers\CategoryTransfer[] $result */
+		$this->assertEquals(1, count($result));
+		$this->assertInstanceOf('\Hitmeister\Component\Api\Transfers\CategoryTransfer', $result[0]);
+		$this->assertEquals(30511, $result[0]->id_category);
+		$this->assertEquals(38441, $result[0]->id_parent_category);
+		$this->assertEquals('schutzfolien-handys', $result[0]->name);
+	}
+
 	/**
 	 * @expectedException \Hitmeister\Component\Api\Exceptions\InvalidArgumentException
 	 */
