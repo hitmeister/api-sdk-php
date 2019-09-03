eb41800ea8e0d2c7641c1697601be5443aa456da
Bastian Böhne <bastian.boehne@hitmeister.de>
2019-09-03 17:54:42 +0200

    PD-780: synced sdk with new field from api

--- src/Transfers/ItemTransfer.php
+++ src/Transfers/ItemTransfer.php
@@ -17,6 +17,7 @@ namespace Hitmeister\Component\Api\Transfers;
  * @property string $real_mgb_article_number
  * @property int $age_rating
  * @property boolean $is_valid
+ * @property int $id_item_uncontaminated
  *
  *
  */
@@ -74,6 +75,11 @@ class ItemTransfer extends AbstractTransfer
     'is_multiple' => false,
   ),
   'is_valid' => 
+  array (
+    'embedded' => false,
+    'is_multiple' => false,
+  ),
+  'id_item_uncontaminated' => 
   array (
     'embedded' => false,
     'is_multiple' => false,
